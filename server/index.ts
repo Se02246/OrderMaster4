2271fca47ecaa7f8ff31c42b06ab03445db3fbb6
Se02246 <sebastianomereu02@gmail.com>
2025-11-02 12:19:20 +0100

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,17 @@
 import 'dotenv/config'; // Importa all'inizio!
 import express from 'express';
-// NON importare più ./auth
 
-// Importa il nuovo middleware di Clerk
-import { clerkMiddleware } from './middleware'; 
+// Importa con {} i file che usano "export const"
+import { clerkMiddleware } from './middleware';
 import { apiRoutes } from './routes';
-import { viteMiddleware } from './vite';
+
+// Importa senza {} il file che usa "export default"
+import viteMiddleware from './vite';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-// Rimuovi il vecchio app.use(session(...)) e setupAuth(app)
-// ...
 
 // Aggiungi il middleware Clerk PRIMA delle tue rotte API
 app.use(clerkMiddleware);

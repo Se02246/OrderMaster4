c4cb8793b7fcbbfeaa9537b544a37eec747bba69
Se02246 <sebastianomereu02@gmail.com>
2025-11-05 14:59:24 +0100

    Svuotato il branch rimuovendo tutti i file

--- server/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { type Request, type Response, type NextFunction } from "express";
-
-export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.status(401).json({ message: "Non autorizzato. Effettua il login." });
-}
